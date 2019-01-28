426c1c3e483fe7c0423ef715dba5c9dab43386f9
CO\MM07860 <shuji_mikami@murata.com>
2019-01-28 16:40:44 +0900

    I2CSlaveの起動までコーディング

--- ControlBox/FW/GPIO_Expander/main.cpp
+++ ControlBox/FW/GPIO_Expander/main.cpp
@@ -1,12 +1,24 @@
 #include "mbed.h"
 
-DigitalOut myled(LED1);
+#define I2C_SDA_PIN dp5
+#define I2C_SCL_PIN dp27
+#define I2C_SAD 0x5E
+#define I2C_FREQ 100000
+
+
 
 int main() {
+    //Start I2C Slave
+    I2CSlave i2cSlave(I2C_SDA_PIN, I2C_SCL_PIN);
+
+    //AddressSetting
+    i2cSlave.address(I2C_SAD);
+
+    //FreqSetting
+    i2cSlave.frequency(I2C_FREQ);
+    
+
+
     while(1) {
-        myled = 1;
-        wait(0.2);
-        myled = 0;
-        wait(0.2);
     }
 }
