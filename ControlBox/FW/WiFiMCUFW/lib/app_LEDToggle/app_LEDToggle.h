b4d4ac342b456b8de83e05bcf3e4d9ec22afb4ee
CO\MM07860 <shuji_mikami@murata.com>
2019-02-07 13:24:09 +0900

    タスクをファイル分け

---
+++ ControlBox/FW/WiFiMCUFW/lib/app_LEDToggle/app_LEDToggle.h
@@ -0,0 +1,7 @@
+#ifndef _APP_LED_TOGGLE_H_
+#define _APP_LED_TOGGLE_H_
+#endif
+
+void LEDToggleTask_initialize();
+void LEDToggleTask_loop();
+
--- ControlBox/FW/WiFiMCUFW/src/main.cpp
+++ ControlBox/FW/WiFiMCUFW/src/main.cpp
@@ -1,18 +1,9 @@
 #include <Arduino.h>
-#include <ESP8266WiFi.h>
+#include "app_LEDToggle.h"
+#include "app_WiFiAPScan.h"
 
-const uint8_t LED_PIN = 16;
-const uint32_t LED_DELAY = 1000;
 
 const uint32_t MESSAGE_DELAY = 1000;
-
-void LEDToggle();
-void LEDToggleTask_initialize();
-void LEDToggleTask_loop();
-
-void WiFiAPScan();
-void WiFiAPScanTask_initialize();
-void WiFiAPScanTask_loop();
 
 void SerialTask_initialize();
 void SerialTask_loop();
@@ -32,47 +23,6 @@ void loop()
   SerialTask_loop();
 }
 
-void LEDToggle()
-{
-  static uint8_t value = LOW;
-
-  digitalWrite(LED_PIN, value);
-
-  if(value == LOW){
-    value = HIGH;
-  }else{
-    value = LOW;
-  }
-}
-
-void LEDToggleTask_initialize()
-{
-  pinMode(LED_PIN, OUTPUT);
-}
-void LEDToggleTask_loop()
-{
-  static uint32_t prevCount = millis();
-
-  uint32_t currentCount = millis();
-
-  if(currentCount - prevCount >= LED_DELAY){
-    LEDToggle();
-    prevCount = currentCount;
-  }
-}
-void WiFiAPScan()
-{
-  WiFi.scanNetworks(true, false, 0, NULL);
-}
-void WiFiAPScanTask_initialize()
-{
-  WiFi.mode(WIFI_STA);
-  WiFi.disconnect();
-}
-void WiFiAPScanTask_loop()
-{
-
-}
 void SerialTask_initialize()
 {
   Serial.begin(115200);
