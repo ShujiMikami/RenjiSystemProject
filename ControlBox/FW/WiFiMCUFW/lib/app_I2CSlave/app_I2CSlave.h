2244b9fbd2936f606b130aecba366bd8e87325a7
ShujiMikami <carbonized.pan@gmail.com>
2019-02-10 15:39:12 +0900

    mainにタスク名を追加

--- ControlBox/FW/WiFiMCUFW/lib/app_I2CSlave/app_I2CSlave.h
+++
@@ -1,6 +0,0 @@
-#ifndef _APP_I2CSLAVE_H_
-#define _APP_I2CSLAVE_H_
-#endif
-
-void I2CSlaveTask_initialize();
-void I2CSlaveTask_loop();
--- ControlBox/FW/WiFiMCUFW/src/main.cpp
+++ ControlBox/FW/WiFiMCUFW/src/main.cpp
@@ -1,46 +1,42 @@
 #include <Arduino.h>
 #include "app_LEDToggle.h"
 #include "app_WiFiAPScan.h"
-//#include "app_I2CSlave.h"
 #include "app_SPIMaster.h"
 
 const uint32_t MESSAGE_DELAY = 1000;
+const String TASK_NAME = "MainTask";
 
-void SerialTask_initialize();
-void SerialTask_loop();
+void MainTask_initialize();
+void MainTask_loop();
 
 void setup() 
 {
   // put your setup code here, to run once:
   LEDToggleTask_initialize();
-  SerialTask_initialize();
-  WiFiAPScanTask_initialize();
-  //I2CSlaveTask_initialize();
+  MainTask_initialize();
   SPIMasterTask_initialize();
-  
 }
 
 void loop()
 {
   // put your main code here, to run repeatedly:
   LEDToggleTask_loop();
-  SerialTask_loop();
-  //I2CSlaveTask_loop();
+  MainTask_loop();
   SPIMasterTask_loop();
 }
 
-void SerialTask_initialize()
+void MainTask_initialize()
 {
   Serial.begin(115200);
 }
-void SerialTask_loop()
+void MainTask_loop()
 {
   static uint32_t prevCount = millis();
 
   uint32_t currentCount = millis();
 
   if(currentCount - prevCount >= MESSAGE_DELAY){
-    Serial.println("Running.");
+    Serial.println("[" + TASK_NAME + "]" + "Running");
     prevCount = currentCount;
   }
 }
