aee4f51f790bb09f65f46c4f5ea1700599775ddd
CO\MM07860 <shuji_mikami@murata.com>
2019-02-20 17:53:01 +0900

    booleanを定義(cubemxではc++しかなかった)

--- ControlBox/FW/MainMCUFW/UserModules/APP_GPIO/Inc/APP_GPIO.h
+++ ControlBox/FW/MainMCUFW/UserModules/APP_GPIO/Inc/APP_GPIO.h
@@ -6,7 +6,13 @@
 #include "stm32f3xx_hal.h"
 #include "gpio.h"
 
+
+typedef enum{
+    TRUE,
+    FALSE
+}boolean_t;
+
 void APP_GPIO_Init();
 void APP_GPIO_Write(GPIO_TypeDef* gpioPort, uint16_t gpioPin, GPIO_PinState gpioState);
-GPIO_PinState APP_GPIO_Read(GPIO_TypeDef* gpioPort, uint16_t gpioPin, bool antiChattering);
+GPIO_PinState APP_GPIO_Read(GPIO_TypeDef* gpioPort, uint16_t gpioPin, boolean_t antiChattering);
 void APP_GPIO_SetChatteringParam(uint32_t period_ms, uint32_t stableCount);
