90bbfe290733a9e90c92817bf563ed94ea826309
ShujiMikami <carbonized.pan@gmail.com>
2019-02-23 02:26:10 +0900

    SPISlaveをmakeに追加

---
+++ ControlBox/FW/MainMCUFW/UserModules/APP_SPI_Slave/Src/APP_SPI_Slave.c
@@ -0,0 +1,27 @@
+#include "APP_SPI_Slave.h"
+#include "cmsis_os.h"
+
+
+static void start_SPI_Read_Task(const void* argument);
+
+void APP_SPI_Slave_Init()
+{
+
+}
+void APP_SPI_Slave_SetData(uint8_t data)
+{
+
+}
+void APP_SPI_SLAVE_GetData(uint8_t* buffer, uint16_t bufferSize)
+{
+
+}
+uint16_t APP_SPI_SLAVE_GetDataCount()
+{
+
+}
+
+void start_SPI_Read_Task(const void* argument)
+{
+
+}
