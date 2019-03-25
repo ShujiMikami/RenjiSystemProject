a6e1524e92e8bafa34e51eba7e320a2e4e01e0b7
CO\MM07860 <shuji_mikami@murata.com>
2019-03-25 11:39:00 +0900

    UARTの送信キューを構造体形式

--- ControlBox/FW/MainMCUFW/UserModules/APP_UART/Src/APP_UART.c
+++ ControlBox/FW/MainMCUFW/UserModules/APP_UART/Src/APP_UART.c
@@ -2,13 +2,19 @@
 #include "cmsis_os.h"
 #include "dma.h"
 
+typedef struct{
+  uint8_t* DataPointer;
+  uint16_t DataLength;
+}TXQueueStruct_t;
+
 #define UART_RX_QUEUE_DEPTH 300
 #define UART_TX_QUEUE_DEPTH 5
 #define UART_RX_QUEUE_SIZE 1
-#define UART_TX_QUEUE_SIZE 20
+#define UART_TX_QUEUE_SIZE sizeof(TXQueueStruct_t)
 #define UART_RX_QUEUE_BUFFER_SIZE (UART_RX_QUEUE_DEPTH * UART_RX_QUEUE_SIZE)
 #define UART_TX_QUEUE_BUFFER_SIZE (UART_TX_QUEUE_DEPTH * UART_TX_QUEUE_SIZE)
 
+#define UART_RECEIVE_BUFFER_SIZE 40
 
 osThreadId UartRXTaskHandle;
 osThreadId UartTXTaskHandle;
@@ -32,7 +38,7 @@ void UARTInit()
 }
 void StartUartRXTask(const void* argument)
 {
-  static uint8_t receiveBuffer[40];
+  static uint8_t receiveBuffer[UART_RECEIVE_BUFFER_SIZE];
 
   HAL_UART_Receive_DMA(&huart1, receiveBuffer, sizeof(receiveBuffer));
 
@@ -47,7 +53,7 @@ void StartUartRXTask(const void* argument)
       dataCount = lastDataPosition - readPosition;
     }
     else if(lastDataPosition < readPosition){//データ書き込み位置がバッファの終端を超えてしまった場合
-      dataCount = lastDataPosition + 40 - readPosition;
+      dataCount = lastDataPosition + UART_RECEIVE_BUFFER_SIZE - readPosition;
     }
     else{//データ書き込み位置に変化がない場合
       //Do nothing
@@ -55,18 +61,18 @@ void StartUartRXTask(const void* argument)
    
     int cnt = 0;
     for(cnt = 0; cnt < dataCount; cnt++){
-      xQueueSendToBack(UartRxQueue, &receiveBuffer[(readPosition + cnt) % 40], 0);
+      xQueueSendToBack(UartRxQueue, &receiveBuffer[(readPosition + cnt) % UART_RECEIVE_BUFFER_SIZE], 0);
     }   
   }
 }
 void StartUartTXTask(const void* argument)
 {
-  char buffer[UART_TX_QUEUE_BUFFER_SIZE];
+  TXQueueStruct_t queueStruct;
 
   while(1){
-    xQueueReceive(UartTxQueue, buffer, portMAX_DELAY);
+    xQueueReceive(UartTxQueue, &queueStruct, portMAX_DELAY);
 
-    HAL_UART_Transmit(&huart1, (uint8_t*)buffer, strlen(buffer), 1000);
+    HAL_UART_Transmit(&huart1, queueStruct.DataPointer, queueStruct.DataLength, 1000);
   }
 }
 int UARTGetReceivedData(uint8_t* buffer, uint16_t bufferLength)
@@ -77,10 +83,16 @@ int UARTGetReceivedData(uint8_t* buffer, uint16_t bufferLength)
   for(cnt = 0; cnt < bufferLength; cnt++){
     xQueueReceive(UartRxQueue, buffer, 0);
   }
-
 }
 void UARTSendData(uint8_t* data, uint16_t dataLength)
-{}
+{
+  TXQueueStruct_t queueStruct;
+
+  queueStruct.DataPointer = data;
+  queueStruct.DataLength = dataLength;
+  
+  xQueueSendToBack(UartTxQueue, &queueStruct, portMAX_DELAY);
+}
 void HAL_UART_TxHalfCpltCallback(UART_HandleTypeDef *huart)
 {
   bufferEdgePassCnt++;
