6e58f1e5955e44724472ce5eca3b61ffe6fd9c12
CO\MM07860 <shuji_mikami@murata.com>
2019-06-21 17:38:52 +0900

    実装続き

--- ControlBox/FW/WiFiMCUFW/lib/UARTCommunication/FormatInfo.h
+++ ControlBox/FW/WiFiMCUFW/lib/UARTCommunication/FormatInfo.h
@@ -8,11 +8,25 @@ typedef unsigned long uint32_t;
 #define DATA_BYTE_SIZE 64
 #define CHECKSUM_BYTE_SIZE 1
 #define FULL_PACKET_SIZE (COMMAND_BYTE_SIZE + DATA_BYTE_SIZE + CHECKSUM_BYTE_SIZE)
+byte GetResponse();
+
 
 //WiFiセットアップモード遷移通知
 const byte COMMANDCODE_WIFI_SETUP_NOTIFICATION = 0x01;
 uint32_t GetSSID();
 uint32_t GetPass();
+
+//HostからのWiFi設定取得通知
+const byte COMMANDCODE_WIFI_SETTING_RECEIVED_NOTIFICATION = 0x02;
+
+//WiFiルータへの接続結果通知
+const byte COMMANDCODE_WIFI_CONNECTION_RESULT = 0x03;
+byte GetStatusCode();
+
+//ケージのステータス要求
+const byte COMMANDCODE_GET_CAGE_STATUS = 0x04;
+
+//ケージのステータス返答
 
 
 
