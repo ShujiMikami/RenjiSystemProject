d47aba0c47729628d85f46057e7aeb64f23a9d7d
CO\MM07860 <shuji_mikami@murata.com>
2019-06-24 15:04:07 +0900

    改名

---
+++ ControlBox/FW/WiFiMCUFW/lib/Test/Test_UARTCom.cpp
@@ -0,0 +1,11 @@
+#include "Test_UARTCom.h"
+#include "UARTCom.h"
+
+void UARTComTest::Setup()
+{
+    UARTCom::Setup();
+}
+void UARTComTest::Loop()
+{
+    UARTCom::Loop();
+}
---
+++ ControlBox/FW/WiFiMCUFW/lib/Test/Test_UARTCom.h
@@ -0,0 +1,13 @@
+#ifndef _TEST_UARTCOM_H_
+#define _TEST_UARTCOM_H_
+
+//UARTComTest
+class UARTComTest{
+public:
+    static void Setup();
+    static void Loop();
+private:
+    UARTComTest();
+};
+
+#endif
