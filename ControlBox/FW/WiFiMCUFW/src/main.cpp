82e602b5a319cc9075744ba3d1006058d08a7915
CO\MM07860 <shuji_mikami@murata.com>
2019-06-25 13:45:10 +0900

    WiFiSetupCommandTestの追加

--- ControlBox/FW/WiFiMCUFW/src/main.cpp
+++ ControlBox/FW/WiFiMCUFW/src/main.cpp
@@ -2,17 +2,25 @@
 
 #include "MainSequencer.h"
 #include "Test_UARTCom.h"
+#include "Test_WiFiSetupCommand.h"
 
 void setup() 
 {
   // put your setup code here, to run once:
-  UARTComTest::Setup();
+  //UARTComTest::Setup();
+  WiFiSetupCommandTest::Setup();
+  
+  
   //MainSequencer::Setup();
 }
 
 void loop()
 {
   // put your main code here, to run repeatedly:
-  UARTComTest::Loop();
+  //UARTComTest::Loop();
+  WiFiSetupCommandTest::Loop();
+
+
+
   //MainSequencer::Loop();
 }
