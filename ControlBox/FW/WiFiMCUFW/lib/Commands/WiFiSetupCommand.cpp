cd6a0eabd889f4dd82a50dcd8ad82d649793a2f4
CO\MM07860 <shuji_mikami@murata.com>
2019-06-25 13:46:02 +0900

    SSID, PASSでのコンストラクタを作成

--- ControlBox/FW/WiFiMCUFW/lib/Commands/WiFiSetupCommand.cpp
+++ ControlBox/FW/WiFiMCUFW/lib/Commands/WiFiSetupCommand.cpp
@@ -12,8 +12,29 @@
 WiFiSetupCommand::WiFiSetupCommand(byte* dataArray, size_t arrayLength) : Command_t(dataArray, arrayLength)
 {
     if(commandContentsValid){//チェックサムOKの場合は, コマンドコードが正しいかチェックする
+        Println(DEBUG_MESSAGE_HEADER + "checkSum is OK");
         commandContentsValid = (commandCode == CommandCode);
+        Println(DEBUG_MESSAGE_HEADER + "Command Validation result = " + String(commandContentsValid));
     }
+}
+WiFiSetupCommand::WiFiSetupCommand(byte* SSID, byte* PASS)
+{
+    dataBuffer[COMMAND_CODE_POS] = CommandCode;
+
+    for(int i = 0; i < SSID_BYTE_SIZE; i++){
+        dataBuffer[SSID_START_POS + i] = SSID[i];
+        Println(DEBUG_MESSAGE_HEADER + "copied " + String(SSID[i]) + " to " + String(SSID_START_POS + i));
+    }
+
+    for(int i = 0; i < PASS_BYTE_SIZE; i++){
+        dataBuffer[PASS_START_POS + i] = PASS[i];
+        Println(DEBUG_MESSAGE_HEADER + "copied " + String(PASS[i]) + " to " + String(PASS_START_POS + i));
+    }
+
+    dataBuffer[CHECKSUM_POS] = calculateChecksum();
+    Println(DEBUG_MESSAGE_HEADER + "calculated checksum = " + String(dataBuffer[CHECKSUM_POS]));
+
+    updateMemberVariable();
 }
 int WiFiSetupCommand::GetSSID(byte* buffer, size_t bufferSize)
 {
