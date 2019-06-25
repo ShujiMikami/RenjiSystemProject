2157f95c5d5655155ece0ad3cdc99fd2858bb353
CO\MM07860 <shuji_mikami@murata.com>
2019-06-25 13:47:02 +0900

    バグ取り

--- ControlBox/FW/WiFiMCUFW/lib/Commands/Command_t.cpp
+++ ControlBox/FW/WiFiMCUFW/lib/Commands/Command_t.cpp
@@ -1,17 +1,25 @@
 #include "Command_t.h"
+#include "DebugPrintf.h"
 
 #define PACKET_LENGTH_WITHOUT_CHECKSUM (FULL_PACKET_SIZE - CHECKSUM_BYTE_SIZE)
 
+bool Command_t::DebugSwitch = false;
+
 Command_t::Command_t(byte* dataArray, size_t arrayLength)
 {
-    for(int i = 0; i < FULL_PACKET_SIZE; i++){
-        if(arrayLength < i + 1){
+    for(size_t i = 0; i < FULL_PACKET_SIZE; i++){
+        if(arrayLength > i){
             dataBuffer[i] = dataArray[i];
         }
         else{
             dataBuffer[i] = 0;
+            Println(DEBUG_MESSAGE_HEADER + "out of data array index : i = " + String(i));
         }
     }
+    updateMemberVariable();
+}
+Command_t::Command_t()
+{
     updateMemberVariable();
 }
 void Command_t::updateMemberVariable()
@@ -20,18 +28,30 @@ void Command_t::updateMemberVariable()
     response = dataBuffer[RESPONSE_POS];
     checkSum = dataBuffer[CHECKSUM_POS];
 
+    Println(DEBUG_MESSAGE_HEADER + "commandCode = " + String(commandCode));
+    Println(DEBUG_MESSAGE_HEADER + "response = " + String(response));
+    Println(DEBUG_MESSAGE_HEADER + "checkSum = " + String(checkSum));
+
+
     commandContentsValid = checkSumValidate();
 }
 bool Command_t::checkSumValidate()
 {
-    bool result = false;
-
+    return (calculateChecksum() == checkSum);
+}
+byte Command_t::calculateChecksum()
+{
     uint16_t sum = 0;
     for(int i = 0; i < PACKET_LENGTH_WITHOUT_CHECKSUM; i++){
         sum += (uint16_t)dataBuffer[i];
     }
 
-    result = ((byte)sum == checkSum);
+    return (byte)sum;
+}
+void Command_t::Println(String message)
+{
+    if(DebugSwitch){
+        PrintfDebugger::Println(message);
+    }
 
-    return result;
 }
