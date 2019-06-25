ea3fd9e7c8e934b9a851ff468b85e495dd9127b4
CO\MM07860 <shuji_mikami@murata.com>
2019-06-25 14:35:29 +0900

    テストに余計なコードを削除

--- ControlBox/FW/WiFiMCUFW/src/main.cpp
+++ ControlBox/FW/WiFiMCUFW/src/main.cpp
@@ -1,17 +1,11 @@
 #include <Arduino.h>
 
-#include "MainSequencer.h"
-#include "Test_UARTCom.h"
-#include "Test_WiFiSetupCommand.h"
-#include "Test_WiFiSettingReceivedCommand.h"
+//#include "MainSequencer.h"
 #include "Test_WiFiRouterConnectionCommand.h"
 
 void setup() 
 {
   // put your setup code here, to run once:
-  //UARTComTest::Setup();
-  //WiFiSetupCommandTest::Setup();
-  //WiFiSettingReceivedCommandTest::Setup();
   WiFiRouterConnectionCommandTest::Setup(); 
   
   //MainSequencer::Setup();
@@ -20,10 +14,6 @@ void setup()
 void loop()
 {
   // put your main code here, to run repeatedly:
-  //UARTComTest::Loop();
-  //WiFiSetupCommandTest::Loop();
-
-
 
   //MainSequencer::Loop();
 }
