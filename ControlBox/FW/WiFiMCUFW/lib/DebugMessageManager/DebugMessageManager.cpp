890c4ed7f250ccdace57af39edcdb55cf5348f73
CO\MM07860 <shuji_mikami@murata.com>
2019-06-26 13:49:18 +0900

    platformioでは, 一つのヘッダに対し, 複数のソースがあるとビルドできないみたい

--- ControlBox/FW/WiFiMCUFW/lib/DebugMessageManager/DebugMessageManager.cpp
+++ ControlBox/FW/WiFiMCUFW/lib/DebugMessageManager/DebugMessageManager.cpp
@@ -1,16 +1,16 @@
-#include "MainSequencer.h"
+#include "DebugMessageManager.h"
 
 //デバッグスイッチを含むモジュールを追加するたびにここにincludeを追記
-#include "Command_t.h"
 #include "WebServerAction.h"
 #include "WiFiHTTPServer.h"
 #include "UARTCom.h"
+#include "Command_t.h"
 
 
-void MainSequencer::debugMessageManage()
+void DebugMessageManager::MessageManage()
 {
     WebServerAction::DebugSwitch = true;
     WiFiHTTPServer::DebugSwitch = true;
     UARTCom::DebugSwitch = true;
-    //Command_t::DebugSwitch = true;
+    Command_t::DebugSwitch = true;
 }
---
+++ ControlBox/FW/WiFiMCUFW/lib/DebugMessageManager/DebugMessageManager.h
@@ -0,0 +1,11 @@
+#ifndef _DEBUG_MESSAGE_MANAGER_H_
+#define _DEBUG_MESSAGE_MANAGER_H_
+
+class DebugMessageManager{
+public:
+    static void MessageManage();
+private:
+    DebugMessageManager();
+};
+
+#endif
