9ba127253467276bbce492da02e871531af055c0
CO\MM07860 <shuji_mikami@murata.com>
2019-06-27 13:04:55 +0900

    Command_tでのSendAndReceive実装

--- ControlBox/FW/WiFiMCUFW/lib/UARTCommunication/UARTCom.cpp
+++ ControlBox/FW/WiFiMCUFW/lib/UARTCommunication/UARTCom.cpp
@@ -120,3 +120,18 @@ void UARTCom::SendDataAndReceive(byte* data, size_t numOfBytesToSend, byte* rece
 
     Println(DEBUG_MESSAGE_HEADER + "received " + String(receivedByteCount) + " bytes");
 }
+Command_t UARTCom::SendDataAndReceive(Command_t command, int timeOut)
+{
+    pSerialForUARTCom->setTimeout(timeOut);
+
+    byte sendData[FULL_PACKET_SIZE];
+    byte receiveBuf[FULL_PACKET_SIZE];
+
+    int commandByteCount = command.GetBytes(sendData, sizeof(sendData));
+
+    if(commandByteCount == FULL_PACKET_SIZE){
+        SendDataAndReceive(sendData, sizeof(sendData), receiveBuf, sizeof(receiveBuf), timeOut);
+    }
+
+    return Command_t(receiveBuf, sizeof(receiveBuf));
+}
--- ControlBox/FW/WiFiMCUFW/lib/UARTCommunication/UARTCom.h
+++ ControlBox/FW/WiFiMCUFW/lib/UARTCommunication/UARTCom.h
@@ -2,6 +2,7 @@
 #define _UARTCOM_H_
 
 #include <Arduino.h>
+#include "Command_t.h"
 
 class UARTCom{
 public:
@@ -9,6 +10,7 @@ public:
     static int GetReceivedData(byte* buffer, size_t bufferSize);
     static void SendData(byte* data, int numOfBytesToSend);
     static void SendDataAndReceive(byte* data, size_t numOfBytesToSend, byte* receiveBuffer, size_t numOfBytesToReceive, int timeOut);
+    static Command_t SendDataAndReceive(Command_t command, int timeOut);
     static void Setup();
     static void Loop();
     static bool DebugSwitch;
